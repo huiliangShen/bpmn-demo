2dfd7e8567f6a34464c3dc04358c78dde4f75cb6
banruo <shl@dataqin.com>
2021-03-05 12:35:31 +0800

    add

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -13,6 +13,7 @@ export function customTranslate(template, replacements) {
     if (!lang) {
         return template
     }
+    setTemps(template)
     // Translate
     const Template = (lang === 'zh' && zh[template]) || template
     // console.log(Replacements, template, replacements)
@@ -20,4 +21,19 @@ export function customTranslate(template, replacements) {
     return Template.replace(/{([^}]+)}/g, function (_, key) {
         return Replacements[key] || '{' + key + '}'
     })
+}
+
+function setTemps(template) {
+    let temps = localStorage.getItem('temps') || ''
+    temps = temps ? temps.split(',') : []
+    if (temps.includes(template)) {
+        return false
+    }
+    const obj = {}
+    temps.push(template)
+    localStorage.setItem('temps', temps.join(','))
+    temps.forEach((e) => {
+        obj[e] = ''
+    })
+    console.log(obj)
 }
